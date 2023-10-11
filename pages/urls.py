a910219da8416de353dc38ef4cf399b4fdd881a7
piet <pprotassow@posteo.de>
2023-10-11 11:59:32 +0200

    test added

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, homePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("home2/", homePageView, name="home2"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from datetime import datetime
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -7,4 +8,18 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+
+def homePageView(request):
+    context = {
+        'key1': 'value1',
+        'key2': 'value2',
+        'key3': 'Hello',
+        'first': [1, 2, 3],
+        'second': [4, 5, 6],
+        'today': datetime.now() 
+    }
+    response = render(request, 'home2.html', context)
+    print('Response:', response)
+    return response
 
